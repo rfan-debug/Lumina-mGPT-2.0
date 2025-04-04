0964289941d23eed195e4aca5fd3303f7cb81034
QIN QI <34205021+ChinChyi@users.noreply.github.com>
2025-04-05 00:02:41 +0800

    Update sample.sh

--- lumina_mgpt/scripts/sample.sh
+++ lumina_mgpt/scripts/sample.sh
@@ -5,5 +5,5 @@ results_dir=samples/
 mkdir -p ${results_dir}
 
 CUDA_VISIBLE_DEVICES=${cuda_number} python generate_examples/generate.py \
---model_path path/to/ckpt \
+--model_path Alpha-VLLM/Lumina-mGPT-2.0 \
 --save_path ${results_dir} --cfg 4.0 --top_k 4096 --temperature 1.0 --width 768 --height 768 --speculative_jacobi --task ${task}
